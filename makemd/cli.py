cb58ee0e27c6321b962a6f804da1f7531e004363
Yoav Ram <yoav@yoavram.com>
2017-07-23 21:55:42 +0300

    verbosity; fix print bug

--- makemd/cli.py
+++ makemd/cli.py
@@ -7,11 +7,12 @@ from bibtexparser.bwriter import BibTexWriter
 from bibtexparser.bibdatabase import BibDatabase
 import click
 
-print = lambda s: click.secho(s, fg='red')
-
-species_pattern = re.compile(
-	r'({\\textless}i{\\textgreater}\w.*?{\\textless}/i{\\textgreater})'
-)
+_print = print
+def print(msg, file=None):
+	if file is None:
+		click.secho(s, fg='red')
+	else:
+		_print(msg, file=file)
 
 
 @click.group()
@@ -22,13 +23,18 @@ def cli():
 @cli.command()
 @click.argument('markdown_input', type=click.File('rt'))
 @click.argument('keys_output', type=click.File('wt'))
-def list(markdown_input, keys_output):
+@click.option('-v/-V', '--verbose/--no-verbose', default=False)
+def list(markdown_input, keys_output, verbose):
 	pattern = re.compile('@([-\w]+\d{4}[a-z]?)')
 	groups = (pattern.findall(line) for line in markdown_input)
 	groups = sum((g for g in groups if g), [])	
 	groups = set(groups)
+	if verbose:		
+		print("Writing {} keys from {} to {}".format(len(groups), 
+			click.format_filename(markdown_input.name), click.format_filename(keys_output)))		
 	for g in groups:
 		print(g, file=keys_output)
+
 
 
 @cli.command()
@@ -40,12 +46,17 @@ def extract(keys_input, bibtex_input, bibtex_output, verbose):
 	lines = keys_input.readlines()
 	citation_keys = (line.strip() for line in lines)
 	if verbose:
-		print("Read {} keys from {}".format(len(lines), keys_input.name))
+		print("Read {} keys from {}".format(len(lines), 
+			click.format_filename(keys_input.name)))
 	main_bib = load_bib(bibtex_input)
 	if verbose:
-		print("Read {} entries from {}".format(len(main_bib.entries), bibtex_input.name))
+		print("Read {} entries from {}".format(len(main_bib.entries), 
+			click.format_filename(bibtex_input.name)))
 
 	out_bib = BibDatabase()
+	species_pattern = re.compile(
+		r'({\\textless}i{\\textgreater}\w.*?{\\textless}/i{\\textgreater})'
+	)
 	for key in citation_keys:
 		e = main_bib.entries_dict[key]		
 		title = e['title']
@@ -61,7 +72,8 @@ def extract(keys_input, bibtex_input, bibtex_output, verbose):
 		e['title'] = title		
 		out_bib.entries.append(e)
 	if verbose:
-		print("Writing {} entries to {}".format(len(out_bib.entries), bibtex_output.name))
+		print("Writing {} entries to {}".format(len(out_bib.entries), 
+			click.format_filename(bibtex_output.name)))
 	writer = BibTexWriter()
 	bibtex_output.write(writer.write(out_bib))
 
